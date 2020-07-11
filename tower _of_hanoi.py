ac847d7f15840353141e021c3132b319257ab77a
Raazeshp96 <raazeshp96@gmail.com>
2020-07-11 23:13:32 +0545

    tower of hanoi solution

---
+++ tower _of_hanoi.py
@@ -0,0 +1,17 @@
+'''
+    Tower of Hanoi problem for ‘n’ number of disks.
+
+'''
+
+
+def TowerOfHanoi(n, from_rod, to_rod, aux_rod):
+    if n == 1:
+        print("Move disk 1 from rod", from_rod, "to rod", to_rod)
+        return
+    TowerOfHanoi(n-1, from_rod, aux_rod, to_rod)
+    print("Move disk", n, "from rod", from_rod, "to rod", to_rod)
+    TowerOfHanoi(n-1, aux_rod, to_rod, from_rod)
+
+
+n = int(input("Enter the no of disk:"))
+TowerOfHanoi(n, 'A', 'C', 'B')
